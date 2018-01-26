630c264eafe12a1523745ec836d3330897c80cdd
Shane Hower <shane.hower@gmail.com>
2018-01-26 13:50:49 -0500

    Update automated_upload_to_s3.py

--- automated_upload_to_s3.py
+++ automated_upload_to_s3.py
@@ -64,11 +64,3 @@ class S3Synchronizer:
             print('Not uploaded. No new files.')
 
 
-while True:
-    s3 = S3Synchronizer('/Users/shanehower/PycharmProjects','my-first-backup-bucket0328',10)
-    bucket = s3.get_files_bucket()
-    folders = s3.get_folder_in_directory()
-    files = s3.get_files_in_directory(folders,bucket)
-    s3.start_upload(files)
-
-
