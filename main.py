a466b03bdc13f18755e601b548ea36fab6b80789
Shane Hower <shane.hower@gmail.com>
2018-01-26 16:53:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from s3sync.py import S3Synchronizer
 
-s3 = S3Sychronizer('/Users/shanehower/PycharmProjects','my-first-backup-bucket0328',2)
+s3 = S3Sychronizer('/Users/shanehower/PycharmProjects','my-first-backup-bucket0328',3600)
 s3.start_upload()
