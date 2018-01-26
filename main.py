e356c414d086987e3452ec79dd093deed1af8298
Shane Hower <shane.hower@gmail.com>
2018-01-26 16:43:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import time
 while True:
     print('sleeping')
     time.sleep(10)
-    s3 = S3Sychronizer('/Users/shanehower/PycharmProjects','my-first-backup-bucket0328',10)
-    path = s3.get_path()
-    bucket = s3.get_files_bucket()
+    s3 = S3Sychronizer('/Users/shanehower/PycharmProjects','my-first-backup-bucket0328',0)
+    path = s3.get_local_files()
+    bucket = s3.get_remote_files()
     files_to_upload = s3.files_not_s3(path,bucket)
-    s3.start_upload(files_to_upload)    
+    s3.start_upload(files_to_upload)
